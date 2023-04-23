94eec35f6b386af7857d017bb5838786c2e92224
Estifanos <estifanosbeyene21@gmail.com>
2023-04-23 09:32:46 -0400

    feat: make default notification date 2 weeks later

--- pages/api/sign-out.ts
+++ pages/api/sign-out.ts
@@ -1,6 +1,6 @@
 import { auth } from "../../lib-server/lucia";
 
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   error?: string;
@@ -10,12 +10,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method !== "POST")
-    return res.status(404).json({ error: "Not found" });
+  switch (req.method) {
+    case "POST":
+      return await POST(req, res);
+    default:
+      return res.status(404).json({ error: "Not found" });
+  }
+}
+
+const POST: NextApiHandler = async (req, res) => {
   const authRequest = auth.handleRequest(req, res);
   const session = await authRequest.validate();
   if (!session) return res.status(401).json({ error: "Unauthorized" });
   await auth.invalidateSession(session.sessionId);
   authRequest.setSession(null);
   return res.redirect(302, "/");
-}
+};
