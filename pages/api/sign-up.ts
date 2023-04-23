a637507782b70f5336193e006aee9123e0ac44de
Estifanos <estifanosbeyene21@gmail.com>
2023-04-23 08:31:14 -0400

    feat(sign-up api): add validation
    validate req body using zod

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -1,8 +1,9 @@
 import { auth } from "../../lib-server/lucia";
+import { z } from "zod";
 import { LuciaError } from "lucia-auth";
 import { Prisma } from "@prisma/client";
 
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   error?: string;
@@ -12,37 +13,57 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method !== "POST")
-    return res.status(404).json({ error: "Not found" });
-  const { username, password } =
-    typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-  if (!username || !password) {
-    return res.status(200).json({
-      error: "Invalid input",
-    });
+  switch (req.method) {
+    case "POST":
+      return await POST(req, res);
+    default:
+      return res.status(404).json({ error: "Not found" });
   }
+}
+
+const POST: NextApiHandler = async (req, res) => {
+  const { body } = req;
+
   try {
-    const user = await auth.createUser({
-      primaryKey: {
-        providerId: "username",
-        providerUserId: username,
-        password,
-      },
-      attributes: {
-        username,
-      },
+    const { username, password } = z
+      .object({
+        username: z.string(),
+        password: z.string(),
+      })
+      .parse(body);
+
+    return res.json({
+      username,
+      password,
     });
-    const session = await auth.createSession(user.userId);
-    const authRequest = auth.handleRequest(req, res);
-    authRequest.setSession(session);
-    return res.redirect(302, "/");
+    // const user = await auth.createUser({
+    //   primaryKey: {
+    //     providerId: "username",
+    //     providerUserId: username,
+    //     password,
+    //   },
+    //   attributes: {
+    //     username,
+    //   },
+    // });
+    //
+    // const session = await auth.createSession(user.userId);
+    // const authRequest = auth.handleRequest(req, res);
+    // authRequest.setSession(session);
+    // return res.redirect(302, "/");
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        error: error.issues,
+        dump: error,
+      });
+    }
     if (
       error instanceof Prisma.PrismaClientKnownRequestError &&
       error.code === "P2002" &&
       error.message?.includes("username")
     ) {
-      return res.status(200).json({
+      return res.status(400).json({
         error: "Username already in use",
       });
     }
@@ -50,14 +71,15 @@ export default async function handler(
       error instanceof LuciaError &&
       error.message === "AUTH_DUPLICATE_KEY_ID"
     ) {
-      return res.status(200).json({
+      return res.status(400).json({
         error: "Username already in use",
       });
     }
     // database connection error
     console.error(error);
-    return res.status(200).json({
+    return res.status(400).json({
       error: "Unknown error occurred",
+      dump: error,
     });
   }
-}
+};
