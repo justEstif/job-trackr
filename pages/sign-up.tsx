da653af6545b92d6ba15ad3334efdc29a3a60a63
Estifanos <estifanosbeyene21@gmail.com>
2023-04-26 14:56:10 -0400

    feat: use authform in sign-up page

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,57 +1,15 @@
 import Link from "next/link";
 import { auth } from "../lib-server/lucia";
-import { useRouter } from "next/router";
 import type { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-
-const validationSchema = z.object({
-  username: z.string({ required_error: "Username is required" }).nonempty(),
-  password: z.string({ required_error: "Password is required" }).nonempty(),
-});
-
-type ValidationSchema = z.infer<typeof validationSchema>;
+import AuthForm from "../components/AuthForm";
 
 const Index = () => {
-  const router = useRouter();
-
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<ValidationSchema>({ resolver: zodResolver(validationSchema) });
-
-  const onSubmit: SubmitHandler<ValidationSchema> = async (data) => {
-    try {
-      const response = await fetch("/api/sign-up", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      if (response.redirected) return router.push(response.url);
-      const result = (await response.json()) as {
-        error: string;
-      };
-      setError("password", { message: result.error });
-    } catch (error) {}
-  };
-
   return (
     <>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="username" {...register("username")} />
-        {errors.username && <p>{errors.username?.message}</p>}
-        <input placeholder="password" {...register("password")} />
-        {errors.password && <p>{errors.password.message}</p>}
-        <button type="submit">Sign In</button>
-      </form>
+      <h2>Sign up</h2>
+      <AuthForm intent={"sign-up"} />
       <Link href="/sign-in" className="link">
-        Sign in
+        Sign in?
       </Link>
     </>
   );
