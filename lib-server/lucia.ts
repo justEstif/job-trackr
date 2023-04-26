b49fdb56e44086be4a1181920f8693165ffdb920
Estifanos <estifanosbeyene21@gmail.com>
2023-04-26 14:30:39 -0400

    fix: improve prisma client, to avoid multiple instances

--- lib-server/lucia.ts
+++ lib-server/lucia.ts
@@ -1,11 +1,11 @@
 import lucia from "lucia-auth";
 import { node } from "lucia-auth/middleware";
-import prisma from "@lucia-auth/adapter-prisma";
-import { PrismaClient } from "@prisma/client";
+import prismaAdapter from "@lucia-auth/adapter-prisma";
 import "lucia-auth/polyfill/node";
+import { prisma } from "./prisma";
 
 export const auth = lucia({
-  adapter: prisma(new PrismaClient()),
+  adapter: prismaAdapter(prisma),
   env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
   middleware: node(),
   transformDatabaseUser: (userData) => {
