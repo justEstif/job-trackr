5bc4778d4139218be6dd52e1e7ddf73727c2d2f9
Estifanos <estifanosbeyene21@gmail.com>
2023-04-28 14:20:32 -0400

    feat: update schema and remove comment
    remove migrations and notifications field in user

--- prisma/migrations/20230426193812_image_optional/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "company" ALTER COLUMN "image_url" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "notification" ALTER COLUMN "notify_date" SET DEFAULT NOW() + interval '14 day';
---
+++ prisma/migrations/20230428181104_remove_user_notification_field/migration.sql
@@ -0,0 +1,106 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('APPLIED', 'INTERVIEWING', 'DENIED', 'DECLINED', 'ARCHIVED');
+
+-- CreateTable
+CREATE TABLE "job" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "interest" INTEGER NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'APPLIED',
+    "description" TEXT NOT NULL,
+    "source" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "company_id" TEXT NOT NULL,
+
+    CONSTRAINT "job_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "company" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image_url" TEXT,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notification" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "notify_date" TIMESTAMP(3) NOT NULL DEFAULT NOW() + interval '14 day',
+    "job_id" TEXT NOT NULL,
+
+    CONSTRAINT "notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth_user" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "auth_user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth_session" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "active_expires" BIGINT NOT NULL,
+    "idle_expires" BIGINT NOT NULL,
+
+    CONSTRAINT "auth_session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth_key" (
+    "id" TEXT NOT NULL,
+    "hashed_password" TEXT,
+    "user_id" TEXT NOT NULL,
+    "primary_key" BOOLEAN NOT NULL,
+    "expires" BIGINT,
+
+    CONSTRAINT "auth_key_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_name_key" ON "company"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_user_id_key" ON "auth_user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_user_username_key" ON "auth_user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_session_id_key" ON "auth_session"("id");
+
+-- CreateIndex
+CREATE INDEX "auth_session_user_id_idx" ON "auth_session"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_key_id_key" ON "auth_key"("id");
+
+-- CreateIndex
+CREATE INDEX "auth_key_user_id_idx" ON "auth_key"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "job" ADD CONSTRAINT "job_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "job" ADD CONSTRAINT "job_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company" ADD CONSTRAINT "company_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notification" ADD CONSTRAINT "notification_job_id_fkey" FOREIGN KEY ("job_id") REFERENCES "job"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth_session" ADD CONSTRAINT "auth_session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth_key" ADD CONSTRAINT "auth_key_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
