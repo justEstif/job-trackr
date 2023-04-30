3de9c953a51bd411727d0c034c6b8d76aa146ebe
Estifanos <estifanosbeyene21@gmail.com>
2023-04-30 11:56:59 -0400

    feat(withUser): add dev and prod middleware
    - pass the username and user id in the cookie
    
    - based of env call either prod or dev env

--- lib-server/middleware/withUser.ts
+++ lib-server/middleware/withUser.ts
@@ -1,8 +1,17 @@
-import type { User } from "lucia-auth";
 import type { NextApiHandler, NextApiResponse } from "next";
 import { auth } from "../lucia";
 
-const withUser = (handler: NextApiHandler): NextApiHandler => {
+const devMiddleware = (handler: NextApiHandler): NextApiHandler => {
+  return (req, res: NextApiResponse) => {
+    if (req.headers.cookie) {
+      return handler(req, res);
+    }
+    return res.status(401).json({ error: "Unauthorized" });
+  };
+};
+
+// use lucia to check if user exists
+const prodMiddleware = (handler: NextApiHandler): NextApiHandler => {
   return async (req, res: NextApiResponse) => {
     const authRequest = auth.handleRequest(req, res);
     const session = await authRequest.validate();
@@ -10,10 +19,22 @@ const withUser = (handler: NextApiHandler): NextApiHandler => {
       return res.status(401).json({ error: "Unauthorized" });
     } else {
       const { user } = await auth.validateSessionUser(session.sessionId);
-      req.body.user = user;
+      if (req.headers.cookie) {
+        req.headers.cookie = `${JSON.stringify(user)};${req.headers.cookie}`;
+      } else {
+        req.headers.cookie = JSON.stringify(user);
+      }
       return handler(req, res);
     }
   };
+};
+
+const withUser = (handler: NextApiHandler): NextApiHandler => {
+  if (process.env.NODE_ENV === "production") {
+    return prodMiddleware(handler);
+  } else {
+    return devMiddleware(handler);
+  }
 };
 
 export default withUser;
--- pages/api/company/index.ts
+++ pages/api/company/index.ts
@@ -1,8 +1,9 @@
 import { auth } from "../../../lib-server/lucia";
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib-server/prisma";
+import withUser from "../../../lib-server/middleware/withUser";
 
-export default async function API(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "GET":
       return await GET(req, res);
@@ -10,21 +11,17 @@ export default async function API(req: NextApiRequest, res: NextApiResponse) {
       return res.status(404).json({ error: "Not found" });
   }
 }
+export default withUser(handler);
 
 const GET: NextApiHandler = async (req, res) => {
-  const authRequest = auth.handleRequest(req, res);
-  const session = await authRequest.validate();
-  if (!session) return res.status(401).json({ error: "Unauthorized" });
-  else {
-    const { user } = await auth.validateSessionUser(session.sessionId);
-    const companies = await prisma.company.findMany({
-      where: { user_id: user.userId },
-      select: {
-        jobs: { select: { id: true } },
-      },
-    });
-    return res.status(200).json({
-      companies,
-    });
-  }
+  const { user } = req.body; // TODO is this bad?
+  const companies = await prisma.company.findMany({
+    where: { user_id: user.userId },
+    select: {
+      jobs: { select: { id: true } },
+    },
+  });
+  return res.status(200).json({
+    companies,
+  });
 };
