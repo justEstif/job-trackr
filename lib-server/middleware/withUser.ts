d695225b7b743fb01e91d45f508d33c71173a942
Estifanos <estifanosbeyene21@gmail.com>
2023-05-02 11:03:10 -0400

    feat: remove testing middleware
    it makes passing different tests difficult
    
    use lucia for all case, and delete user after running tests

--- lib-server/middleware/withUser.ts
+++ lib-server/middleware/withUser.ts
@@ -1,22 +1,7 @@
 import type { NextApiHandler, NextApiResponse } from "next";
 import { auth } from "../lucia";
 
-/**
- * @description middleware to be used in testing only
- */
-const testMiddleware = (handler: NextApiHandler): NextApiHandler => {
-  return (req, res: NextApiResponse) => {
-    if (req.headers.cookie) {
-      return handler(req, res);
-    }
-    return res.status(401).json({ error: "Unauthorized" });
-  };
-};
-
-/**
- * @description middleware to authorize request using lucia
- */
-const prodMiddleware = (handler: NextApiHandler): NextApiHandler => {
+const withUser = (handler: NextApiHandler): NextApiHandler => {
   return async (req, res: NextApiResponse) => {
     const authRequest = auth.handleRequest(req, res);
     const session = await authRequest.validate();
@@ -30,14 +15,6 @@ const prodMiddleware = (handler: NextApiHandler): NextApiHandler => {
       return handler(req, res);
     }
   };
-};
-
-const withUser = (handler: NextApiHandler): NextApiHandler => {
-  if (process.env.TESTING === "true") {
-    return testMiddleware(handler);
-  } else {
-    return prodMiddleware(handler);
-  }
 };
 
 export default withUser;
