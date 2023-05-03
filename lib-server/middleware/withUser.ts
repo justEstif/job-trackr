267d0a86743501f8f9a791e1358e2cce1710ebae
Estifanos <estifanosbeyene21@gmail.com>
2023-05-03 13:35:36 -0400

    feat(withUser): add userCookie variable

--- lib-server/middleware/withUser.ts
+++ lib-server/middleware/withUser.ts
@@ -9,9 +9,10 @@ const withUser = (handler: NextApiHandler): NextApiHandler => {
       return res.status(401).json({ error: "Unauthorized" });
     } else {
       const { user } = await auth.validateSessionUser(session.sessionId);
+      const userCookie = JSON.stringify(user);
       req.headers.cookie
-        ? (req.headers.cookie = `${JSON.stringify(user)};${req.headers.cookie}`)
-        : (req.headers.cookie = JSON.stringify(user));
+        ? (req.headers.cookie = `${userCookie};${req.headers.cookie}'`)
+        : (req.headers.cookie = `${userCookie};`);
       return handler(req, res);
     }
   };
